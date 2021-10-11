4d731b4bf9a804811e66b7334b341a62357c9132
meesvdent <meesvdent@gmail.com>
2021-10-11 23:46:00 +0200

    working example

--- simple_genetic_algorithm.py
+++ simple_genetic_algorithm.py
@@ -10,6 +10,9 @@ class Chromosome():
         self.sequence = np.array(sequence)
         self.pm = pm
 
+    def __str__(self):
+        return str(self.sequence)
+
     def crossover(self, other):
         point = random.randrange(0, len(self.sequence))
         self_parent = self.sequence[0:point]
@@ -31,4 +34,58 @@ class Population():
         self.pm = pm
         self.pc = pc
         self.lambdaa = lambdaa
+        self.children = []
+
+    def __str__(self):
+        string = ""
+        for individual in self.individuals:
+            string += (str(individual) + "\n")
+        return string
+
+    def match_up(self, k):
+        matches = []
+        random_order = random.sample(list(self.individuals), k=len(self.individuals))
+        while len(random_order) > 2:
+            matches.append((random_order[0], random_order[1]))
+            random_order.pop(0)
+            random_order.pop(0)
+        return matches
+
+    def crossover(self):
+        matches = self.match_up(2)
+        for match in matches:
+            self.children.append(match[0].crossover(match[1]))
+
+    def mutate_children(self):
+        mutated = []
+        for child in self.children:
+            mutated.append(child.mutate())
+        self.children = mutated
+
+    def append_children(self):
+        self.individuals = np.concatenate((self.individuals, self.children))
+
+    def remove_children(self):
+        self.children = []
+
+    def select(self):
+        remove = random.sample(range(len(self.individuals)), len(self.individuals) - self.lambdaa)
+        self.individuals = np.delete(self.individuals, remove)
+
+    def print_individuals(self):
+        for individual in self.individuals:
+            print(individual)
+
+    def run(self, iterations=5):
+        n = 0
+        while n < iterations:
+            self.crossover()
+            self.mutate_children()
+            self.append_children()
+            self.remove_children()
+            self.select()
+            print("Iteration ", n+1)
+            print(self)
+            n += 1
+
 
--- test_simple_genetic_algorithm.py
+++ test_simple_genetic_algorithm.py
@@ -1,6 +1,6 @@
 # Testing Chromosome class methods
 
-from simple_genetic_algorithm import Chromosome
+from simple_genetic_algorithm import Chromosome, Population
 import random
 import numpy as np
 
@@ -14,15 +14,28 @@ def test_chromosome():
     print(child.sequence)
 
 
+
 def test_mutate(chromosome):
     return chromosome.mutate()
 
 def test_crossover(father, mother):
     return father.crossover(mother)
 
+def test_population():
+    chrom_one = Chromosome([False, False, True, True, False, False], pm = 0.2)
+    chrom_two = Chromosome([False, False, True, True, False, False], pm = 0.2)
+    chrom_three = Chromosome([False, False, True, True, False, False], pm = 0.2)
+    chrom_four = Chromosome([False, False, True, True, False, False], pm = 0.2)
+    chrom_five = Chromosome([False, False, True, True, False, False], pm = 0.2)
+    chrom_array = np.array([chrom_one, chrom_two, chrom_three, chrom_four, chrom_five])
+    population = Population(np.array(chrom_array), pm=0.1, pc=1.0, lambdaa=4)
+    print(population.match_up(2))
+
+    population.run(iterations=5)
+
 def main():
     test_chromosome()
-
+    test_population()
 
 if __name__ == "__main__":
     main()
